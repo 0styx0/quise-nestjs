9002c0bee0e30cc420852f71d8d99aee67561f25
Styx Meiseles <git@0styx0.xyz>
2025-09-10 14:42:21 -0400

    feat(products): protect createProducts endpoint

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,14 @@
 
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+    // format errors in gql friendly terms
+    getRequest(context: ExecutionContext): Request {
+        const ctx = GqlExecutionContext.create(context);
+        return ctx.getContext().req;
+    }
+}
--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -2,6 +2,8 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ProductsService } from './products.service';
 import { Product } from './product.entity';
 import { CreateProductInput } from 'src/graphql';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Resolver(() => Product)
 export class ProductsResolver {
@@ -12,6 +14,7 @@ export class ProductsResolver {
     return this.productService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation('createProducts')
   async createProducts(
     @Args('products') products: CreateProductInput[],
